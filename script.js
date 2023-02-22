93156498ed96ce1efb42bfcec1f17062f8901389
BarakaFrite <mathieu69009@gmail.com>
2023-02-22 23:18:22 +0100

    Check who win between user and computer

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function getComputerChoice(){
     let a = Math.floor(Math.random() * 3);
     return possibleChoice[a];
 }
-console.log(getComputerChoice());
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
 
 let repeatChoice = true;
 let cancelChoice = true;
+let playerSelection;
 
 
 const validChoicesRegex = /^\s*(?:rock|paper|scissors)\s*$/gmi;
@@ -26,7 +28,7 @@ function validate(field,regex){
 
 while (repeatChoice && cancelChoice){
     console.log(repeatChoice + '  ' + cancelChoice);
-    let playerSelection = prompt('Rock, Paper or Scissors?');
+    playerSelection = prompt('Rock, Paper or Scissors?');
     console.log('playerSelection : ' + playerSelection);
     if(playerSelection == null){
         console.warn('You choose nothing. Reload to play again.');
@@ -41,4 +43,29 @@ while (repeatChoice && cancelChoice){
     }
 
 }
-
+console.log('playerSelection2 : ' + playerSelection);
+function checkWinner(userInput,computerInput){
+    if(userInput.toUpperCase() == computerInput.toUpperCase()){
+        console.log ('Tie !')
+    }else if (userInput.toUpperCase() == 'ROCK'){
+        if (computerInput.toUpperCase() == 'SCISSORS'){
+            console.log ('WIN !!')
+        }else{
+            console.log ('LOSE !!')
+        }
+    }else if(userInput.toUpperCase() == 'SCISSORS'){
+        if (computerInput.toUpperCase() == 'PAPER'){
+            console.log ('WIN !!')
+        }else{
+            console.log ('LOSE !!')
+        }
+    }else{
+        if (computerInput.toUpperCase() == 'ROCK'){
+            console.log ('WIN !!')
+        }else{
+            console.log ('LOSE !!')
+        }
+    }
+}
+console.log('test : '+ playerSelection + ' '+ computerChoice);
+checkWinner(playerSelection,computerChoice);
