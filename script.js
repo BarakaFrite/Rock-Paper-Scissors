a0411120da9f76055dca29e2e3a9bf51ac8e02a7
BarakaFrite <mathieu69009@gmail.com>
2023-02-22 22:51:11 +0100

    control user input

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 const possibleChoice = ['Paper','Rock','Scissors'];
+
 function getComputerChoice(){
     let a = Math.floor(Math.random() * 3);
     return possibleChoice[a];
 }
 console.log(getComputerChoice());
+
+let repeatChoice = true;
+let cancelChoice = true;
+
+
+const validChoicesRegex = /^\s*(?:rock|paper|scissors)\s*$/gmi;
+
+//validation function
+
+function validate(field,regex){
+    if(regex.test(field)){ 
+        a = 'valid';
+    }else{
+        a='invalid';
+    } 
+    console.log( 'a : ' + a);
+    return a  
+};
+
+while (repeatChoice && cancelChoice){
+    console.log(repeatChoice + '  ' + cancelChoice);
+    let playerSelection = prompt('Rock, Paper or Scissors?');
+    console.log('playerSelection : ' + playerSelection);
+    if(playerSelection == null){
+        console.warn('You choose nothing. Reload to play again.');
+        cancelChoice = false;
+    }else{
+        let checkChoice = validate(playerSelection,validChoicesRegex);
+        if (checkChoice == 'valid'){
+            repeatChoice = false;
+        }else{    
+            console.warn('You choose '+ playerSelection + ' instead of Rock, Paper or Scissors.Please try again');
+        }
+    }
+
+}
+
