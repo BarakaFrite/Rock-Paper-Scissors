9241a5468774dd91a9d3aa8411dbd97c4c332bc0
BarakaFrite <mathieu69009@gmail.com>
2023-02-23 21:52:48 +0100

    debug regex and adding round for game. + Final winner + possibility to tie

--- script.js
+++ script.js
@@ -1,71 +1,100 @@
 const possibleChoice = ['Paper','Rock','Scissors'];
 
 function getComputerChoice(){
-    let a = Math.floor(Math.random() * 3);
-    return possibleChoice[a];
+    let x = Math.floor(Math.random() * 3);
+    return possibleChoice[x];
 }
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
-
-let repeatChoice = true;
-let cancelChoice = true;
+let computerChoice;
 let playerSelection;
 
 
-const validChoicesRegex = /^\s*(?:rock|paper|scissors)\s*$/gmi;
+const validChoicesRegex = /^\s*(?:rock|paper|scissors)\s*$/mi;
 
 //validation function
 
 function validate(field,regex){
-    if(regex.test(field)){ 
-        a = 'valid';
-    }else{
-        a='invalid';
-    } 
-    console.log( 'a : ' + a);
-    return a  
+    regex.lastIndex= 0;
+    return regex.test(field)
 };
 
-while (repeatChoice && cancelChoice){
-    console.log(repeatChoice + '  ' + cancelChoice);
-    playerSelection = prompt('Rock, Paper or Scissors?');
-    console.log('playerSelection : ' + playerSelection);
-    if(playerSelection == null){
-        console.warn('You choose nothing. Reload to play again.');
-        cancelChoice = false;
-    }else{
-        let checkChoice = validate(playerSelection,validChoicesRegex);
-        if (checkChoice == 'valid'){
-            repeatChoice = false;
-        }else{    
-            console.warn('You choose '+ playerSelection + ' instead of Rock, Paper or Scissors.Please try again');
-        }
-    }
-
-}
-console.log('playerSelection2 : ' + playerSelection);
-function checkWinner(userInput,computerInput){
+function playRound(userInput,computerInput){
+    
     if(userInput.toUpperCase() == computerInput.toUpperCase()){
         console.log ('Tie !')
     }else if (userInput.toUpperCase() == 'ROCK'){
         if (computerInput.toUpperCase() == 'SCISSORS'){
-            console.log ('WIN !!')
+            console.log ('WIN !!');
+            scoreUser++;
         }else{
-            console.log ('LOSE !!')
+            console.log ('LOSE !!');
+            scoreComputer++;
         }
     }else if(userInput.toUpperCase() == 'SCISSORS'){
         if (computerInput.toUpperCase() == 'PAPER'){
-            console.log ('WIN !!')
+            console.log ('WIN !!');
+            scoreUser++;
         }else{
-            console.log ('LOSE !!')
+            console.log ('LOSE !!');
+            scoreComputer++;
         }
     }else{
         if (computerInput.toUpperCase() == 'ROCK'){
-            console.log ('WIN !!')
+            console.log ('WIN !!');
+            scoreUser++;
         }else{
-            console.log ('LOSE !!')
+            console.log ('LOSE !!');
+            scoreComputer++;
         }
     }
 }
-console.log('test : '+ playerSelection + ' '+ computerChoice);
-checkWinner(playerSelection,computerChoice);
+
+//main
+let scoreUser = 0;
+let scoreComputer =0;
+let repeatChoice = true;
+let cancelChoice = true;
+
+
+for(let i=0; i< 5;i ++){
+    let checkChoice = false;
+    console.log('___________________________________');
+    console.log('Round : '+ i);
+    computerChoice = getComputerChoice();
+    console.log('computerChoice : ' + computerChoice);
+    while (repeatChoice && cancelChoice){
+        console.log(repeatChoice + '  ' + cancelChoice);
+        playerSelection = prompt('Rock, Paper or Scissors?');
+        console.log('playerSelection : ' + playerSelection);
+        if(playerSelection == null){
+            console.warn('You choose to stop the game :(. Reload to play again.');
+            cancelChoice = false;
+        }else{
+            checkChoice = validate(playerSelection,validChoicesRegex);
+            console.log ('checkChoice : '+ checkChoice);
+            if (checkChoice == true){
+                repeatChoice = false;
+            }else{    
+                console.warn('You choose '+ playerSelection + ' instead of Rock, Paper or Scissors.Please try again');
+            }
+        }
+    
+    }
+    repeatChoice = true;
+    cancelChoice = true;
+    console.log('test : '+ i +' '+ playerSelection + ' '+ computerChoice);
+
+    playRound(playerSelection,computerChoice);
+    console.log ('User score : ' + scoreUser + ' Computer score : ' + scoreComputer);
+
+}
+let winnerIs;
+if(scoreUser > scoreComputer){
+    winnerIs = 'User';
+}else if(scoreUser < scoreComputer){
+    winnerIs = 'computer';
+}else{
+    winnerIs = 'no one';
+}
+console.log('*****************************');
+console.warn('Winner is : ' + winnerIs);
+console.log('*****************************');
